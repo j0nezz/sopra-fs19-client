ae2b19c64d448ef98aa1ccc369ece30693297e73
Jonas Brunner <jonas.brunner@outlook.com>
2019-02-25 15:47:45 +0100

    Updated node modules

--- src/components/shared/routeProtectors/LoginGuard.js
+++ src/components/shared/routeProtectors/LoginGuard.js
@@ -7,7 +7,7 @@ import { Redirect } from "react-router-dom";
  */
 export const LoginGuard = props => {
   if (!localStorage.getItem("token")) {
-    return props.children;
+    return props.children; // returns Login Component
   }
   // if user is already logged in, redirects to the main /app
   return <Redirect to={"/game"} />;
